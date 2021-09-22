1fed51c6c7e8d619585d151bd4c2ef64bd43b3d0
ranj-ana <86224765+ranj-ana@users.noreply.github.com>
2021-09-23 00:44:59 +0530

    Create c2.cpp

---
+++ Week 2/c2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void find_triplet(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int start=0;
+        int last=n-1;
+        while(start<last)
+        {
+            if(arr[start]+arr[last]==arr[i])
+            {
+
+                cout<<start+1<<","<<last+1<<","<<i+1<<endl;
+                return;
+            }
+            else if(arr[i]>arr[start]+arr[last])
+                start++;
+            else
+                last--;
+        }
+    }
+    cout<<"No Sequence Found"<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        find_triplet(arr,n);
+    }
+    return 0;
